9b9917eef7c39e2fd9ff503c575e87c45f4d91fe
isaak velasquez <isaak@kubeTests.isaakube>
2026-03-10 21:50:03 -0600

    row of led code setup

--- led.c
+++ led.c
@@ -2,15 +2,45 @@
 #include <util/delay.h>
 
 int main(void) {
+
+  /* PINS:
+     PB0
+     PB1
+     PB2
+     PB3
+   */
+
+  
+  // Goal: toggle pin 8x
   // Set direction register output
-  DDRD = DDRD | (1 << PD2);
+   DDRB = DDRB | (1 << PB0);
+   DDRB = DDRB | (1 << PB1);
+   DDRB = DDRB | (1 << PB2);
+   DDRB = DDRB | (1 << PB3);
 
+      //  _delay_ms(100);
+
+  // turn pin 8 ON
+    
   while(1) {
-    PORTD = PORTD | (1 << PD2);
-    _delay_ms(100);
+    // toggle the pin
+    PORTB |= (1 << PB0);
+    _delay_ms(200);
+    PORTB |= (1 << PB1);
+    _delay_ms(200);
+    PORTB |= (1 << PB2);
+    _delay_ms(200);
+    PORTB |= (1 << PB3);
+    _delay_ms(200);
+    //PORTB ^= (1 << PB0);
 
-    PORTD = PORTD & ~(1 << PD2);
-
-    _delay_ms(100);
-  }
+    PORTB &= ~(1 << PB0);
+    PORTB &= ~(1 << PB1);
+    PORTB &= ~(1 << PB2);
+    PORTB &= ~(1 << PB3);
+    _delay_ms(200);
+    
+   }
+  
 }
+
